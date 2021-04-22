b505ee5f96ca3d6fa29da311ff028598c0ac0f1d
achapadev <83032999+achapadev@users.noreply.github.com>
2021-04-22 16:31:37 -0500

    Render full year with javascript in html

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+
+import React from "react";
+import ReactDom from "react-dom";
+
+const name = "Adrian Chapa";
+var d = new Date();
+
+ReactDom.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {`${new Date().getFullYear()}`}</p>
+  </div>,
+  document.querySelector("#root")
+);
